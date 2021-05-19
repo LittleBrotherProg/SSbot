0cfd40a448cafd09e1548639521368472621b656
yarik60 <78476810+yarik60@users.noreply.github.com>
2021-05-19 16:18:30 +0300

    Автоматизация карусели

--- bot3.py
+++ bot3.py
@@ -7,13 +7,77 @@ import json
 import mysql.connector
 from mysql.connector import Error
 import re
+import random
 
-GROUP_ID = '146384697'
-GROUP_TOKEN = '85a8d32f22314f81cb27945010b31f2b59b00c9dcca40c39f2a8f1a395653509eba9a1ae4bdd0ff2812cb'
+buttons = {
+                "action": {
+                    "type": "open_link",
+                    "link": "https://vk.com/littlebr0therr",
+                    "label": "От 3000 куб|1400 руб",
+                    "payload": "{}"
+                },
+
+            },
+
+bt = buttons
+
+
+carousel = {
+    "type": "carousel",
+    "elements": [{
+        "photo_id": "-146384697_457239210",
+        "title": "Вентиляция приточная",
+        "description": "Вентиляция приточная",
+        "action": {
+            "type": "open_link",
+            "link": "https://vk.com/littlebr0therr"
+        },
+        "buttons": [
+            {
+                "action": {
+                    "type": "open_link",
+                    "link": "https://vk.com/littlebr0therr",
+                    "label": "До 1500 куб|800 руб",
+                    "payload": "{}"
+                },
+
+            },
+            {
+                "action": {
+                    "type": "open_link",
+                    "link": "https://vk.com/littlebr0therr",
+                    "label": "До 3000 куб|1000 руб",
+                    "payload": "{}"
+                },
+
+            },
+
+            {
+                "action": {
+                    "type": "open_link",
+                    "link": "https://vk.com/littlebr0therr",
+                    "label": "От 3000 куб|1400 руб",
+                    "payload": "{}"
+                },
+
+            },
+        ]
+    },
+    ]
+}
+
+
+
+
+carousel = json.dumps(carousel, ensure_ascii=False).encode('utf-8')
+carousel = str(carousel.decode('utf-8'))
+
+GROUP_ID = '204661014'
+GROUP_TOKEN = '22117b50d967969e1e3d42997ef4cebba7aec9482cbaed68cf13a6e9551de367fe3ebb9b588df4cd504e8'
 API_VERSION = '5.120'
 f_toggle: bool = False
 
-vk_session = VkApi(token=GROUP_TOKEN, api_version=API_VERSION)
+vk_session = VkApi(token=GROUP_TOKEN)
 vk = vk_session.get_api()
 longpoll = VkBotLongPoll(vk_session, group_id=GROUP_ID)
 
@@ -64,8 +128,6 @@ def buttns_name(database, list):
     return keyboard7
 
 
-
-
 def send_message(user_id, message, keyboard=None):
     post = {
         "user_id": user_id,
@@ -81,16 +143,16 @@ def send_message(user_id, message, keyboard=None):
     vk_session.method("messages.send", post)
 
 
-def edit_message(message, keyboard):
-    post = {
-        "random_id": 0,
-        'peer_id': event.obj.peer_id,
-        "message": message,
-        "conversation_message_id": event.obj.conversation_message_id,
-        "keyboard": (keyboard_1 if f_toggle else keyboard).get_keyboard()
-    }
-
-    vk_session.method("messages.edit", post)
+# def edit_message(message, keyboard):
+#     post = {
+#         "random_id": 0,
+#         'peer_id': event.obj.peer_id,
+#         "message": message,
+#         "conversation_message_id": event.obj.conversation_message_id,
+#         "keyboard": (keyboard_1 if f_toggle else keyboard).get_keyboard()
+#     }
+#
+#     vk_session.method("messages.edit", post)
 
 
 def main_menu(buttons_name):
@@ -99,6 +161,12 @@ def main_menu(buttons_name):
         but = f"{button}"
         keybrd.append(but)
     return keybrd
+
+
+def send_message_carusel(user_id, text, keyboard=None, template=None):
+    vk_session.method("messages.send", {"user_id": user_id, "message": text,
+                                        "random_id": random.randint(-9223372036854775807, 9223372036854775807),
+                                        "keyboard": keyboard, "template": template})
 
 
 for event in VkBotLongPoll(vk_session, group_id=GROUP_ID).listen():
@@ -110,11 +178,7 @@ for event in VkBotLongPoll(vk_session, group_id=GROUP_ID).listen():
         first_name = user_get['first_name']
 
         if text == 'Диагностика оборудования за 1 точку':
-           keyboard_1 = VkKeyboard(one_time=False, inline=True)
-           keyboard_1.add_callback_button(label='Добавить красного ',
-                                           color=VkKeyboardColor.PRIMARY,
-                                           payload={"type": "my_own_100500_type_edit"})
-           send_message(user_id, "Сервисные услуги", keyboard_1)
+            send_message_carusel(user_id, "Карусель!", template=carousel)
 
 
         elif text == "Сервис":
--- button_test.py
+++ button_test.py
@@ -4,8 +4,8 @@ from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.keyboard import VkKeyboard
 import random
 import json
+import re
 from vk_api import VkApi
-
 
 carousel = {
     "type": "carousel",
@@ -16,52 +16,92 @@ carousel = {
         "action": {
             "type": "open_link",
             "link": "https://vk.com/littlebr0therr"
+
         },
-        "buttons": [
-            {
+
+    }
+    ]
+}
+hg = {
                 "action": {
                     "type": "open_link",
                     "link": "https://vk.com/littlebr0therr",
                     "label": "До 1500 куб|800 руб",
                     "payload": "{}"
                 },
-
-            },
-            {
-                "action": {
-                    "type": "open_link",
-                    "link": "https://vk.com/littlebr0therr",
-                    "label": "До 3000 куб|1000 руб",
-                    "payload": "{}"
-                },
-
-            },
-            {
-                "action": {
-                    "type": "open_link",
-                    "link": "https://vk.com/littlebr0therr",
-                    "label": "От 3000 куб|1400 руб",
-                    "payload": "{}"
-                },
-
-            },
-        ]
-    },
-    ]
 }
+rp = ['с', 'п', 'и', 'с', 'о', 'к']
 
-carousel = json.dumps(carousel, ensure_ascii=False).encode('utf-8')
-carousel = str(carousel.decode('utf-8'))
+lp = {"type": "carousel", }
+
+
+
+
+def car(id_photo, title, description, action):
+    g = 3
+    h = 0
+    while g != h:
+        lp["elements"] = [{"photo_id": id_photo,"title": title,"description": description,"action": action,"buttons": [hg]}]
+        h += 1
+
+    return lp
+
+
+
+
+kok = car("asdasd", "asdasda", "asdasda", "asdasda")
+
+kok = car("asdasd", "asdasda", "asdasda", "asdasda")
+
+kok = car("asdasd", "asdasda", "asdasda", "asdasda")
+print(lp)
+lp["elements"][0]["buttons"].append(hg)
+
+print(lp)
+
+# print(carousel)
+# bt = {"buttons": [{
+#                 "action": {
+#                     "type": "open_link",
+#                     "link": "https://vk.com/littlebr0therr",
+#                     "label": "До 1500 куб|800 руб",
+#                     "payload": "{}"
+#                 },
+# }]
+#
+#             },
+#
+#
+#
+
+# ln = (",".join("Python"))
+#
+# bt = dict()
+# print(bt)
+# carousel.update([bt])
+# # carousel['elements:[{buttons'].append(bt)
+# print(carousel)
+# # act = [{
+# #                 "action": {
+# #                     "type": "open_link",
+# #                     "link": "https://vk.com/littlebr0therr",
+# #                     "label": "До 1500 куб|800 руб",
+# #                     "payload": "{}"
+# #                 },
+# #
+# #             },]
+#
+#
+# # bt["buttons"].append(act)
+# # re.sub("[(|)]", "", bt)
+#
+kok = json.dumps(kok, ensure_ascii=False).encode('utf-8')
+kok = str(kok.decode('utf-8'))
 
 GROUP_ID = '146384697'
 GROUP_TOKEN = '85a8d32f22314f81cb27945010b31f2b59b00c9dcca40c39f2a8f1a395653509eba9a1ae4bdd0ff2812cb'
 API_VERSION = '5.103'
 
-# vk_session = VkApi(token=GROUP_TOKEN, api_version=API_VERSION)
-# vk = vk_session.get_api()
-# longpoll = VkBotLongPoll(vk_session, group_id=GROUP_ID)
-
-vk_session = VkApi(token=GROUP_TOKEN)
 vk = vk_api.VkApi(token=GROUP_TOKEN)
 
 vk._auth_token()
@@ -71,10 +111,8 @@ vk.get_api()
 longpoll = VkBotLongPoll(vk, GROUP_ID)
 
 
-def send_message(user_id, text, keyboard=None, template=None):
-    vk_session.method("messages.send", {"user_id": user_id, "message": text,
-                                "random_id": random.randint(-9223372036854775807, 9223372036854775807),
-                                "keyboard": keyboard, "template": template})
+def send_message_carusel(user_id, text, keyboard=None, template=None):
+    vk.method("messages.send", {"user_id": user_id, "message": text,"random_id": random.randint(-9223372036854775807, 9223372036854775807),"keyboard": keyboard, "template": template})
 
 
 for event in longpoll.listen():
@@ -90,5 +128,4 @@ for event in longpoll.listen():
             keyboard.add_openlink_button("URL кнопка", "Кнопка")
 
         elif event.object.message["text"] == "3":
-            send_message(user_id, "Карусель!", template=carousel)
-
+            send_message_carusel(user_id, "Карусель!", template=kok)
